506b44dd65049336ebcbafa3271323f13d249865
lokeshkumargurram <gurramlokeshkumar@gmail.com>
2019-02-16 15:12:38 +0530

    hi

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cache1="c2";
+this.addEventListener("install",function(event){
+	event.waitUntil(
+
+		caches.open(cache1).then(cache=>{
+           cache.addALL([
+           	])
+		})
+		);
+
+});
+
+this.addEventListener("fetch",function(event){
+	event.respondWidth(
+caches.open(cache1).then(function(cache){
+
+	return cache.match(event.request).then(function(res){
+	    return res|| fetch(event.request).then(function(reqres){
+	    cache.put(event.request,reqres.clone());
+       })
+    })
+ })
+
+       )
+})	
